587618f5cb863806d2cb7d8480863003848f62a6
Kamil S <41943469+TooxPL@users.noreply.github.com>
2019-03-16 21:58:04 +0100

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,11 @@
-const path                 = require('path');
-const webpack              = require('webpack');
-const node_externals       = require('webpack-node-externals');
-const html_web_pack_plugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: {
-    server: './server.js',
+    index : './src/index.js'
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].js'
-  },
-  target: 'node',
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-  externals: [ node_externals() ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [{ loader: "html-loader" }]
-      }
-    ]
-  },
-  plugins: [
-    new html_web_pack_plugin({
-      template: "./index.html",
-      filename: "./index.html",
-      excludeChunks: [ 'server' ]
-    })
-  ]
-}
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
